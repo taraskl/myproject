df03c23a99fd463e2f29e1c963156b21120b8e86
taraskl <taras.kliuchnyk@gmail.com>
2015-11-16 22:28:50 +0200

    rename project

--- myprojekt/myprojekt/context_processors.py
+++
@@ -1,4 +0,0 @@
-from .settings import PORTAL_URL
-
-def students_proc(request):
-	return {'PORTAL_URL': PORTAL_URL}
--- myprojekt/students/admin.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.contrib import admin
-from .models import Student, Group
-from .models import MonthJournal
-
-# Register your models here.
-admin.site.register(MonthJournal)
-admin.site.register(Student)
-admin.site.register(Group)

987655f7445bf2b2d2cddf3b694dddf48861c378
taraskl <taras.kliuchnyk@gmail.com>
2015-11-16 22:50:34 +0200

    change gitignore

---
+++ students/migrations/0003_auto_20150502_1500.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('students', '0002_auto_20150502_1414'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='student',
+            name='student_group',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, verbose_name='\u0413\u0440\u0443\u043f\u0430', to='students.Group', null=True),
+        ),
+        migrations.AlterField(
+            model_name='group',
+            name='leader',
+            field=models.OneToOneField(null=True, on_delete=django.db.models.deletion.SET_NULL, blank=True, to='students.Student', verbose_name='\u0421\u0442\u0430\u0440\u043e\u0441\u0442\u0430'),
+        ),
+    ]
